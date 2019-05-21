1fff05b888f9f7250402968cebf5dd548f0d6ddb
Jon <jon.d@windowslive.com>
2019-05-21 20:23:43 +1200

    Created convenience methods for Stock Endpoint.

--- Cin7ApiWrapper/Endpoints/StockEndpoint.cs
+++ Cin7ApiWrapper/Endpoints/StockEndpoint.cs
@@ -1,6 +1,7 @@
 ﻿using Cin7ApiWrapper.Common;
 using Cin7ApiWrapper.Endpoints.Base;
 using Cin7ApiWrapper.Models;
+using System.Collections.Generic;
 
 namespace Cin7ApiWrapper.Endpoints
 {
@@ -11,5 +12,31 @@ namespace Cin7ApiWrapper.Endpoints
         {
 
         }
+
+        public IEnumerable<StockUnit> ForBranchId(int branchId)
+        {
+            return Find(where: $"branchId={branchId}");
+        }
+
+        /// <summary>
+        /// Find Stock for a Code (SKU)
+        /// </summary>
+        /// <param name="code"></param>
+        /// <returns></returns>
+        public IEnumerable<StockUnit> ForCode(string code)
+        {
+            return Find(where: $"code='{code}'");
+        }
+
+        /// <summary>
+        /// Find Stock for a Barcode (UPC)
+        /// </summary>
+        /// <param name="barcode"></param>
+        /// <returns></returns>
+        public IEnumerable<StockUnit> ForBarcode(string barcode)
+        {
+            return Find(where: $"barcode='{barcode}'");
+        }
+
     }
 }
